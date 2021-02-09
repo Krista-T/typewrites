e2ded28da8ed6e249fd73ccd029050b71c37d613
krista <krista.tomicic@gmail.com>
2021-02-09 22:30:15 +0100

    final commit

--- typewriter.js
+++ typewriter.js
@@ -2,23 +2,27 @@
 window.addEventListener("DOMContentLoaded", typeWriterText);
 const title = document.querySelector(".typewritten").textContent;
 let textDisplay = "";
+document.querySelector("#type2").classList.add("hide");
 
 function typeWriterText() {
   for (let i = 0; i < title.length; i++) {
     setTimeout(() => {
       textDisplay += title.charAt(i);
       document.querySelector(".typewritten").innerHTML = textDisplay;
-      console.log(textDisplay);
+      // console.log(textDisplay);
     }, i * 250);
   }
-  startTimer();
 }
 
-//terrible solution
-let time;
-function startTimer() {
-  console.log("this is terrible solution");
-  time = setInterval(() => {
-    location.reload();
-  }, 14000);
-}
+setTimeout(() => {
+  document.querySelector("#type2").classList.remove("hide");
+  const title2 = document.querySelector("#type2").textContent;
+  let textDisplay2 = "";
+  for (let i = 0; i < title2.length; i++) {
+    setTimeout(() => {
+      textDisplay2 += title2.charAt(i);
+      document.querySelector("h2").innerHTML = textDisplay2;
+      // console.log(textDisplay2);
+    }, i * 250);
+  }
+}, 14000);
